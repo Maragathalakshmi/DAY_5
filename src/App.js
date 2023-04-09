9b4fbe35d05e166203c734bd250cb5b87f4e32fa
Maragathalakshmi <108513716+Maragathalakshmi@users.noreply.github.com>
2023-04-09 12:22:26 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Day5 from './Day5';
+import Errorboundary from './Errorboundary';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <h1>MY FAVOURITES</h1>
+      <Errorboundary>
+        <Day5 a={["Chocalate", "Icecream", "Fruits", "Cookies"]} />
+      </Errorboundary>
     </div>
   );
 }
 
-export default App;
+export default App
